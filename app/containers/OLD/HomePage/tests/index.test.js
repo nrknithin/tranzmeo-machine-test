d6d4b41bffed74e1763a99d796164337f9bdb77f
nithin <nrknithin@gmail.com>
2022-06-25 08:47:20 +0530

    moving old pages to new dir

--- app/containers/OLD/HomePage/tests/index.test.js
+++ app/containers/OLD/HomePage/tests/index.test.js
@@ -10,8 +10,8 @@ import { browserHistory } from 'react-router-dom';
 
 import { HomePage, mapDispatchToProps } from '../index';
 import { changeUsername } from '../actions';
-import { loadRepos } from '../../App/actions';
-import configureStore from '../../../configureStore';
+import { loadRepos } from '../../../App/actions';
+import configureStore from '../../../../configureStore';
 
 describe('<HomePage />', () => {
   let store;
